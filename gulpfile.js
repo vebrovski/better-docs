df9ef03ce54f3e987904135cfe179d3422d1d34a
Matija Veber <m.veber@sportradar.com>
2021-09-13 17:36:21 +0200

    gulpfile changes for v4.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const DOCS_COMMAND = process.env.DOCS_COMMAND || 'yarn docs'
 const DOCS_OUTPUT = process.env.DOCS_OUTPUT || '../docs'
 
 const gulp = require('gulp')
-const sass = require('gulp-sass')
+const sass = require('gulp-sass')(require('sass'))
 const autoprefixer = require('gulp-autoprefixer')
 const run = require('gulp-run')
 const babel = require('gulp-babel')
@@ -12,18 +12,19 @@ const concat = require('gulp-concat')
 const path = require('path')
 const browserSync = require('browser-sync').create()
 
-gulp.task('sass', () => {
-  gulp.src('styles/app.sass')
+function styles() {
+  return gulp.src('styles/app.sass')
     .pipe(sass({
       outputStyle: 'compressed',
     }))
     .pipe(autoprefixer())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('static/styles'))
-})
+}
+exports.styles = styles
 
-gulp.task('js', () => {
-  gulp.src(path.join('scripts/', '*.js'), { base: 'app' })
+function scripts() {
+  return gulp.src(path.join('scripts/', '*.js'), { base: 'app' })
     .pipe(concat('app.js'))
     .pipe(babel({
       presets: ['@babel/env'],
@@ -31,34 +32,38 @@ gulp.task('js', () => {
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('static/scripts'))
-})
+}
+exports.scripts = scripts
 
-gulp.task('docs', function() {
+function docs() {
   return run(`cd .. && ${DOCS_COMMAND}`).exec()
-})
+}
+exports.docs = docs
 
-gulp.task('watch', () => {
-  gulp.watch('styles/**/*.sass', ['sass', 'docs'])
-  gulp.watch('scripts/**/*.js', ['js', 'docs'])
-  gulp.watch('tmpl/**/*.tmpl', ['docs'])
-  gulp.watch('publish.js', ['docs'])
+function watch() {
+  gulp.watch('styles/**/*.sass', gulp.series(styles, docs))
+  gulp.watch('scripts/**/*.js', gulp.series(scripts, docs))
+  gulp.watch('tmpl/**/*.tmpl', gulp.series(docs))
+  gulp.watch('publish.js', gulp.series(docs))
   if (process.env.DOCS) {
     const array = [
       ...process.env.DOCS.split(','),
       ...process.env.DOCS.split(',').map(src => '!' + src.replace('**/*', 'node_modules/**/*'))
     ]
     console.log(array)
-    gulp.watch(array, ['docs'])
+    gulp.watch(array, gulp.series(docs))
   }
-})
+}
+exports.watch = watch
 
-gulp.task('sync', () => {
+function sync() {
   browserSync.init({
     server: {
       baseDir: DOCS_OUTPUT
     }
   })
   gulp.watch(`${DOCS_OUTPUT}/*`).on('change', browserSync.reload)
-})
+}
+exports.sync = sync
 
-gulp.task('default', ['sass', 'js', 'docs', 'watch', 'sync'])
+exports.default = gulp.parallel(styles, scripts, docs, watch, sync)
